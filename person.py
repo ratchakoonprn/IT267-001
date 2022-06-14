efc1c2251b2206a7b20e683486ff1af8d995a21d
ratchakoonprn <ratchakoon.prn@outlook.com>
2022-06-14 13:05:27 +0700

    modifiy person

--- person.py
+++ person.py
@@ -1,5 +1,7 @@
 class Person:
+    country = "Thailand" #class variable
     def __init__(self,name,gender,profession,hours) -> None:
+        #instance variables
         self.name = name
         self.gender = gender
         self.profession = profession
@@ -27,3 +29,20 @@ jon.study()
 
 lalisa = Person("Lalisa","Female","Korean Singer",13)
 lalisa.work()
+
+print(f"Class Variable: {Person.country}")
+print(f"Instance Variable: {lalisa.country}")
+
+#assign value
+lalisa.country = "Korea"
+print("------")
+print(f"Class Variable: {Person.country}")
+print(f"Instance Variable: {lalisa.country}")
+print(f"Instance Variable: {jon.country}")
+
+# assign class variable
+Person.country = "England"
+print("*******")
+print(f"Class Variable: {Person.country}")
+print(f"Instance Variable: {lalisa.country}")
+print(f"Instance Variable: {jon.country}")
