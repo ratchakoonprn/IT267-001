35792481b1185693c0d187a273b7eb98e13e3bf1
ratchakoonprn <ratchakoon.prn@outlook.com>
2022-06-20 15:48:11 +0700

    import cafe

---
+++ oop2/modulepackage/call_cafe_module2.py
@@ -0,0 +1,13 @@
+import cafe
+#แสดงรายการขนมของร้าน
+dessert_menu = cafe.Dessert()
+print(dessert_menu.show_dessert())
+
+#แสดงรายการเครื่องดื่มของร้าน
+drink_menu = cafe.Drink()
+print(drink_menu.show_drink())
+
+#เพิ่มรายการเครื่องดื่ม
+drink_menu.add_drink('juice')
+drink_menu.add_drink('smoothy')
+print(drink_menu.show_drink())
