23f98a6523933d330f12b43c99f9227177abbd43
ratchakoonprn <ratchakoon.prn@outlook.com>
2022-06-21 11:42:51 +0700

    package cusorder

---
+++ oop2/cusorder/customer.py
@@ -0,0 +1,4 @@
+class Customer:
+    def __init__(self) -> None:
+        self.name = ''
+        self.address = ''
---
+++ oop2/cusorder/order.py
@@ -0,0 +1,11 @@
+class Order:
+    def __init__(self) -> None:
+        self.date = ''
+        self.status = ''
+    
+    def cal_tax(self):
+        pass
+    def cal_total(self):
+        pass
+    def calc_total_weight(self):
+        pass
