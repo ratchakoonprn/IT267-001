7e0b2e6a31df63f31d53c76e547359a4472b97e3
ratchakoonprn <ratchakoon.prn@outlook.com>
2022-06-27 10:51:49 +0700

    multiple inheritance

---
+++ oop2/mutiple_inheritance/country.py
@@ -0,0 +1,32 @@
+from geographic import Geographic
+from temperature import Temperature
+
+class Country(Geographic,Temperature):
+    def __init__(self,name,area,pop) -> None:
+        self.name = name
+        self.area = area
+        self.population = pop
+    
+    def getpopulationdensity(self):
+        return self.population / self.area
+    
+    def showdetail(self):
+        #ชื่อประเทศ
+        print(f'Country: {self.name}')
+        
+        #สถานที่ตั้ง latitude และ longitude
+        print(f'Location:{self.getcordinate()}')
+        
+        #แสดงขนาดพื้นที่, จำนวนประชากร และความหนาแน่นของประชากร
+        print(f'Population: {self.population:,d}')
+        print(f'Area: {self.area:,.2f}')
+        print(f'Density: {self.getpopulationdensity():,.2f}')
+        
+        #Time Zone, Climate, Temperature, Weather
+        print(f'Time Zone: {self.gettimezone()}')
+        print(f'Climate: {self.getclimate()}')
+        print(f'Temperture(C): {self.getcelcius():.2f}')
+        print(f'Temperature(F): {self.getfahrenheit():.2f}')
+        print(f'Weather: {self.getweather()}')
+
+        print('*************************')
---
+++ oop2/mutiple_inheritance/country_called.py
@@ -0,0 +1,14 @@
+from country import Country
+
+#create countries
+#Thailand 13.7649136,100.5360959
+#population = 70,078,203
+#area = 513,120
+#temp = 32C
+
+c1 = Country('Thailand',513120,70078203)
+c1.setcordinate(13.7649136,100.5360959)
+c1.setcelcius(32)
+c1.showdetail()
+
+#create 2 countries
