67395cfc99430fde500ef4a3dd18c0bac6f5fe2b
ratchakoonprn <ratchakoon.prn@outlook.com>
2022-07-04 09:14:36 +0700

    modify country init

--- oop2/mutiple_inheritance/country.py
+++ oop2/mutiple_inheritance/country.py
@@ -2,7 +2,14 @@ from geographic import Geographic
 from temperature import Temperature
 
 class Country(Geographic,Temperature):
+    """def __init__(self,name,area,pop) -> None:
+        self.name = name
+        self.area = area
+        self.population = pop"""
     def __init__(self,name,area,pop) -> None:
+        #super().__init__()
+        Geographic.__init__(self)
+        Temperature.__init__(self)
         self.name = name
         self.area = area
         self.population = pop
